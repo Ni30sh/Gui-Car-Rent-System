e03f7f4c62aa779d1e672a0c38a47093513c4410
Ni30shkumar <addy2166@gmail.com>
2023-07-16 00:44:51 +0530

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+car = Tk()
+car.title("Car Agency")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+car.geometry("500x500+400+120")
+car.mainloop()
