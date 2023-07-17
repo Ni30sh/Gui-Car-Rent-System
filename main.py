598006713453517cdc14e1f23f502efb74ad29ca
Ni30shkumar <addy2166@gmail.com>
2023-07-17 17:12:19 +0530

    first commit

--- main.py
+++ main.py
@@ -5,45 +5,41 @@ class car_rent_system:
         self.Total_cars = 100
 
     def car_on_rent(self):
-        var = messagebox.askyesno("Notification", "Cars in Agency :" + str(self.Total_cars)+ " \nTo confirm Y/N....",)
+        var = messagebox.askyesno("Notification", "Cars in Agency :" + str(self.Total_cars) + " \n To confirm Yes/No....",)
         if var == True:
             car1 = Tk()
             car1.title("Costumer Portal")
             car1.config(bg="red")
             car1.geometry("500x300")
-            label2 = Label(car1, text="Your Name", font=("times", 14, "bold"))
-            label2.place(x=15, y=60, height=30, width=140)
-            label3 = Label(car1, text="Your Aadhar ", font=("times", 14, "bold"))
-            label3.place(x=15,y=100,height=30,width=140)
-            label4 = Label(car1, text="Required Car ", font=("times", 14, "bold"))
-            label4.place(x=15,y=145,height=30,width=140)
-            label5 = Label(car1, text="How Many Days ", font=("times", 14, "bold"))
-            label5.place(x=15,y=188,height=30,width=140)
+            label2 = Label(car1, text="Your Name", font=("times", 15, "bold"),anchor="w",bg="red")
+            label2.place(x=15, y=60, height=30, width=150)
+            Frame(car1,bg="black").place(x=15,y=89,height=2,width=100)
+            label3 = Label(car1, text="Your Aadhar ", font=("times", 14, "bold"),anchor="w")
+            label3.place(x=15,y=100,height=30,width=150)
+            Frame(car1, bg="black").place(x=15, y=120, height=2, width=100)
+            label4 = Label(car1, text="Required Car ", font=("times", 14, "bold"),anchor="w")
+            label4.place(x=15,y=145,height=30,width=150)
+            Frame(car1, bg="black").place(x=15, y=169, height=2, width=100)
+            label5 = Label(car1, text="How Many Days ", font=("times", 14, "bold"),anchor="w")
+            label5.place(x=15,y=188,height=30,width=150)
+            Frame(car1, bg="black").place(x=15, y=190, height=2, width=100)
 
             entry1 =  Entry(car1,font=("chillers",15, "bold"))
             entry1.place(x=200,y=60,height=30,width=250)
+            entry2 =  Entry(car1,font=("chillers",15, "bold"))
+            entry2.place(x=200,y=100,height=30,width=250)
+            entry3 =  Entry(car1,font=("chillers",15, "bold"))
+            entry3.place(x=200,y=145,height=30,width=250)
+            entry4 =  Entry(car1,font=("chillers",15, "bold"))
+            entry4.place(x=200,y=188,height=30,width=250)
 
-
-
-
-
-
-
-
-            # Label(car1,text="To Continue.......").place(x=5,y=90,height=40,width=190)
-             # Button(car1,text="Yes",font=("times",11,"bold"),bg="blue",fg="white",
-            # activebackground="white",activeforeground="black",).place(x=20,y=190,height=30,width=60,)
-            # Button(car1,text="No",font=("times",11,"bold"),bg="blue",fg="white",
-            # activebackground="white",activeforeground="black",).place(x=190,y=190,height=30,width=60,)
+            button = Button(car1,text="Sumit",font=("times",20,"bold"),bg="blue",fg="white",activebackground="white",activeforeground="black")
+            button.place(x=180,y=240,height=40,width=140)
             car1.mainloop()
     def car_deposit(self):
        pass
     def potal_exite(self):
         pass
-
-
-
-
 
 car = Tk()
 car.title("Car Agency")
