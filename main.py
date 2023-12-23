c361083ce49e1f90db37aa62790c8a4c1e9ff0d9
Ni30shkumar <addy2166@gmail.com>
2023-12-24 00:22:01 +0530

    half

--- main.py
+++ main.py
@@ -2,42 +2,40 @@ import time
 from tkinter import *
 from tkinter import messagebox
 import mysql.connector
+import pandas as pd 
 class car_rent_system:
     def __init__(self):
         self.Total_cars = 200
     def car_on_rent(self):
 
-        var = messagebox.askyesno("Notification", "Cars in Agency :" + str(self.Total_cars) + " \n To confirm Yes/No....",)
+        var = messagebox.askyesno("Notification", "Cars in Agency :"
+                                  + str(self.Total_cars) + " \n To confirm Yes/No....",)
         if var == True:
 
             def data_sumit(*args):
                 global  conn, cur
-                NAME_val = name.get()
-                AADHAR_val = aadhar.get()        # self.bill_printing(NAME, AADHAR, CARS, DAYS, )
-                CARS_val = cars.get()
-                DAYS_val = days.get()
+                NAME_val = entry1.get()
+                AADHAR_val = entry2.get()        # self.bill_printing(NAME, AADHAR, CARS, DAYS, )
+                CARS_val = entry3.get()
+                DAYS_val = entry4.get()
                 DATE_val = time.strftime("%d-%m-%Y")
+
                 use_database = "USE CarRentalSystem"
                 cur.execute(use_database)
                 cur = conn.cursor()
-                # var1 = "INSERT INTO AgencyDetails VALUES (%s,%s,%s,%s,%s)"
-                # var2 = (NAME_val, AADHAR_val, CARS_val,DAYS_val, DATE_val )
-                cur.execute("INSERT INTO AgencyDetails VALUES (%s,%s,%s,%s,%s)",(NAME_val, AADHAR_val, CARS_val, DAYS_val, DATE_val))
+                cur.execute(
+                    "INSERT INTO AgencyDetails (CUSTOMER_NAME, AADHAR, CAR, NO_OF_DAYS, DATE) VALUES (%s, %s, %s, %s, %s)",
+                    (NAME_val, AADHAR_val, CARS_val, DAYS_val, DATE_val))
                 conn.commit()
                 messagebox.showinfo("Bill",
                                     f"Name : {NAME_val} \n Aadhar : {AADHAR_val} \n No of Cars : {CARS_val}"
                                     f" \n No of Days : {DAYS_val} \n date : {DATE_val}")
-                # def bill_printing(self, *data):
-                # print_bill = f"""
-                #     customer Name = {NAME_val}
-                #     aadhar = {AADHAR_val}
-                #     cars = {CARS_val}
-                #     days = {DAYS_val}"""
-                # file_name = AADHAR_val + ".txt"
-                # p = open(file_name, "w")
-                # p.write(print_bill)
-                # p.close()
-                # print_bill()
+                # Create a text file
+                
+                file_name = f"{NAME_val}.txt"
+                with open(file_name, "w") as file:
+                    file.write(
+                        f"Customer Name: {NAME_val}\nAadhar No : {AADHAR_val}\nNo of Cars on rent: {CARS_val}\nHow many Days: {DAYS_val}\nDate: {DATE_val}")
 
             car1 = Tk()
             car1.title("Costumer Portal")
@@ -65,10 +63,10 @@ class car_rent_system:
             entry1 = Entry(car1,textvariable=name,font=("chillers",15, "bold"),bd = 0)
             entry1.place(x=200,y=60,height=30,width=250)
             entry2 = Entry(car1,textvariable=aadhar,font=("chillers",15, "bold"),bd = 0)
-            entry2.place(x=200,y=100,height=30,width=250)
-            entry3 = Entry(car1,textvariable=cars,font=("chillers",15, "bold"),bd = 0)
-            entry3.place(x=200,y=145,height=30,width=250)
-            entry4 = Entry(car1,textvariable=days,font=("chillers",15, "bold"),bd = 0)
+            entry2.place(x=200,y=100,height=30, width=250)
+            entry3 = Entry(car1,textvariable=cars, font=("chillers",15, "bold"),bd = 0)
+            entry3.place(x=200,y=145,height=30,  width=250)
+            entry4 = Entry(car1,textvariable=days, font=("chillers",15, "bold"),bd = 0)
             entry4.place(x=200,y=188,height=30,width=250)
 
             button = Button(car1,text="Submit",font=("times",20,"bold"),bg="blue",fg="white",
@@ -101,10 +99,10 @@ def data_base():
         var1 = "USE CarRentalSystem"
         cur.execute(var1)
         var2 = """CREATE TABLE AgencyDetails (
-                     CUSTOMER_NAME VARCHAR(255), 
-                     AADHAR BIGINT, 
-                     CAR VARCHAR(255), 
-                     NO_OF_DAYS VARCHAR(255),
+                     CUSTOMER_NAME VARCHAR(10),
+                      AADHAR bigint,
+                     CAR VARCHAR(33),
+                     NO_OF_DAYS VARCHAR(33),
                      DATE VARCHAR(255) )"""
         cur.execute(var2)
         conn.close()
@@ -136,5 +134,4 @@ button3.place(x=160,y=330,height=80,width=180)
 
 data_base()
 car.mainloop()
-
 
